6666b41604215dfeaa9e7a1f2e951d0a7ef02041
Carlos <thehandyowl.io@gmail.com>
2017-05-20 12:57:08 +0200

    Ruta signUp básica

---
+++ routes/api_v1/signUp.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var express = require('express');
+var router = express.Router();
+
+router.post( '/’, function (req, res, next) {
+    console.log(req.body);
+    res.json( { success:true, result: req.body } );
+});
+
+module.exports = router;
